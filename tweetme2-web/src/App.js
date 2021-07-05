0c532dadf83df6b1db7447267c22c45dd4a31e8a
Guilherme bertho luiz <guilhermebluiz@hotmail.com>
2021-07-05 18:43:29 -0300

    action Btn

--- tweetme2-web/src/App.js
+++ tweetme2-web/src/App.js
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 
-function loadTweets (callback) {
+function loadTweets(callback) {
   const xhr = new XMLHttpRequest()
   const method = 'GET' // "POST"
   const url = "http://localhost:8000/api/tweets/"
@@ -11,23 +11,40 @@ function loadTweets (callback) {
   xhr.responseType = responseType
   xhr.open(method, url)
   xhr.onload = function() {
-   callback(xhr.response, xhr.status)
+    callback(xhr.response, xhr.status)
   }
   xhr.onerror = function (e) {
     console.log(e)
-    callback({'message': 'The request was an error'}, 400)
+    callback({"message": "The request was an error"}, 400)
   }
   xhr.send()
 }
 
+
+function ActionBtn(props) {
+  const {tweet, action} = props
+  const className = props.className ? props.className : 'btn btn-primary btn-sm'
+  return action.type === 'like' ? <button className={className}>{tweet.likes} Likes</button> : null
+}
+
+function Tweet(props) {
+  const {tweet} = props
+  const className = props.className ? props.className : 'col-10 mx-auto col-md-6'
+  return <div className={className}>
+      <p>{tweet.id} - {tweet.content}</p>
+      <div className='btn btn-group'>
+        <ActionBtn tweet={tweet} action={{type: "like"}}/>
+        <ActionBtn tweet={tweet} action={{type: "unlike"}}/>
+      </div>
+  </div>
+}
+
 function App() {
-  const [tweets, setTweets] = useState([{content: 123}])
-
-
+  const [tweets, setTweets] = useState([])
+ 
   useEffect(() => {
     const myCallback = (response, status) => {
-      console.log(response, status)
-      if (status === 200) {
+      if (status === 200){
         setTweets(response)
       } else {
         alert("There was an error")
@@ -40,10 +57,13 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          {tweets.map((tweet, index)=>{
-           return <li>{tweet.content}</li>
-          })}
+          Edit <code>src/App.js</code> and save to reload.
         </p>
+        <div>
+          {tweets.map((item, index)=>{
+            return <Tweet tweet={item} className='my-5 py-5 border bg-white text-dark' key={`${index}-{item.id}`} />
+          })}
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
