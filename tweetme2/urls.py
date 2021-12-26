27200d890f6ac3293a7abad20b173dd012924f2a
Guilherme bertho luiz <guilhermebluiz@hotmail.com>
2021-12-26 10:58:06 -0300

    latest changes

--- tweetme2/urls.py
+++ tweetme2/urls.py
@@ -15,7 +15,7 @@ from tweets.views import ( home_view,
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view),
-    path('react/', TemplateView.as_view(template_name='react.html')),
+    path('react/', TemplateView.as_view(template_name='react_via_dj.html')),
     path('tweets', tweet_list_view),
     path('create-tweet', tweet_create_view),
     path('tweets/<int:tweet_id>', tweet_detail_view),
